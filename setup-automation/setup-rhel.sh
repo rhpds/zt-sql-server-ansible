a672d9b666696e88641778f46cc7bb44522d31c1
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-09-18 13:22:25 -0400

    removed instruqt scripting

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,14 +1,4 @@
 !#/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-   echo "Waiting for Instruqt to finish booting the VM"
-   sleep 1
-done
-
-yum remove -y google-rhui-client-rhel8.noarch
-yum clean all
-subscription-manager config --rhsm.manage_repos=1
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
 
 touch /root/post-run.log
 
@@ -64,24 +54,3 @@ sed -i -e "s/host1/localhost/g" site.yml &>> /root/post-run.log
 #yum -y install ansible python3 &>> /root/post-run.log
 echo "Installing ansible-core" >> /root/post-run.log
 dnf -y install ansible-core
-
-#Create a done file to signal we have finished
-touch /root/post-run.log.done
-
-n=1
-GREEN='\033[0;32m' 
-NC='\033[0m' # No Color
-
-while [ ! -f /root/post-run.log.done ] ;
-do
-      if test "$n" = "1"
-      then
-	    clear
-            n=$(( n+1 ))	 # increments $n
-      else
-	    printf "."
-      fi
-      sleep 2
-done
-clear
-echo -e "${GREEN}Ready to start your scenario${NC}"
