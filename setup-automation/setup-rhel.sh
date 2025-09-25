a776a6ee636dacc7df74b51ec0f17c6309d1c2a4
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-09-25 06:54:52 -0400

    removed instruqt-specific code

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -2,10 +2,6 @@
 
 touch /root/post-run.log
 
-#wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/site.yml &>> /root/post-run.log
-#wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/ansible.cfg &>> /root/post-run.log
-#wget https://raw.githubusercontent.com/rhel-labs/learn-katacoda/master/sql-server-ansible/InstPubs.sql &>> /root/post-run.log
-
 # Creating ansible config on host
 echo "Creating site.yml" >> /root/post-run.log
 tee -a ~/site.yml << EOF
@@ -41,9 +37,6 @@ ssh_args=
 EOF
 
 
-echo "adding ansible repo" >> /root/post-run.log
-#subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms &>> /root/post-run.log
-
 echo "Setting hostname env" >> /root/post-run.log
 export HOSTNAME=`hostname --all-fqdns` &>> /root/post-run.log
 
@@ -51,10 +44,5 @@ echo "Setting hostname as localhost in site.yml"
 sed -i -e "s/host1/localhost/g" site.yml &>> /root/post-run.log
 #sed -i -e "s/host1/$HOSTNAME/g" site.yml &>> /root/post-run.log
 
-#echo "installing ansible and pip3" >> /root/post-run.log
-#yum -y install ansible python3 &>> /root/post-run.log
 echo "Installing ansible-core" >> /root/post-run.log
 dnf -y install ansible-core
-
-sudo curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/10/mssql-server-preview.repo
-sudo yum install -y mssql-server
